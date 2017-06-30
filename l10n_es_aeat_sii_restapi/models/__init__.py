c143032b40af234b73dacc9a243afabc9f4fdd99
Ioan Galan <ioan@studio73.es>
2017-06-30 13:18:10 +0200

    [IMP] l10n_es_aeat_sii_restapi: Poder buscar por el codigo en el modelo "aeat.sii.mapping.registration.keys"

--- l10n_es_aeat_sii_restapi/models/__init__.py
+++ l10n_es_aeat_sii_restapi/models/__init__.py
@@ -3,3 +3,4 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import account_invoice_import
+from . import aeat_sii_mapping_registration
---
+++ l10n_es_aeat_sii_restapi/models/aeat_sii_mapping_registration.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# (c) 2017 Consultoría Informática Studio 73 S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api, _
+
+
+class AeatSiiMappingRegistrationKeys(models.Model):
+    _inherit = 'aeat.sii.mapping.registration.keys'
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        args = args or []
+        recs = self.browse()
+        if name:
+            recs = self.search([('code', operator, name)] + args, limit=limit)
+
+        if not recs:
+            recs = super(AeatSiiMappingRegistrationKeys, self).name_search(name, args, operator, limit)
+            return recs
+
+        return recs.name_get()
