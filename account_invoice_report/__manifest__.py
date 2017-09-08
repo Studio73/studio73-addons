556c9c275696b59350e1a167081be654169740ab
Pablo Fuentes <pablo@studio73.es>
2017-09-08 16:59:32 +0200

    [REF] account_invoice_report: Funcionalidad pasada al nuevo módulo debrand_account_invoice_mail

--- account_invoice_report/__manifest__.py
+++ account_invoice_report/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Account Invoice Report',
-    'version': '10.0.0.1.0',
+    'version': '10.0.0.2.0',
     'sequence': 145,
     'category': 'Account',
     'author': 'Consultoría Informática Studio 73 S.L.',
@@ -14,11 +14,10 @@ Account invoice report
 Custom Qweb report, because Odoo's one is too ugly...
 
 """,
-    'depends': ['account', 'debrand_mail_layout'],
+    'depends': ['account', 'debrand_account_invoice_mail'],
     'data': [
         'views/res_company_view.xml',
         'views/report_invoice.xml'
     ],
     'installable': True,
-    'application': False,
 }
