ddf10af2b0c29eac6c8ec5dc71e3185ac93b5ba3
Abraham Anes <abraham@studio73.es>
2017-11-16 11:41:48 +0100

    [ADD] l10n_es_aeat_sii_specific_intracommunity_transactions: Creado modulo par la tramitacion al SII de determiandas operaciones intracomunitarias.

---
+++ l10n_es_aeat_sii_specific_intracommunity_transactions/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# (c) 2017 Studio73 - Abraham Anes <abraham@studio73>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Suministro Inmediato de Información - Determinadas Operaciones Intracomunitarias",
+    "version": "8.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://odoo-community.org/",
+    "author": "Consultoría Informática Studio 73 S.L., ",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es_aeat_sii",
+    ],
+    "data": [
+        'views/account_specific_intracommunity_transactions_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    "application": False,
+    "installable": True,
+}
