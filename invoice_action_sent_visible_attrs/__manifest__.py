dc3425f52cbf68bdbe563e3f0bf5dd8dd0156d3a
Ioan Galan <ioan@studio73.es>
2018-01-08 12:12:38 +0100

    [ADD] invoice_action_sent_visible_attrs: Mostrar el botón 'Enviar por correo electrónico' de las facturas en los estados 'draft', 'open' y 'paid'

---
+++ invoice_action_sent_visible_attrs/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Invoice action sent visible by state',
+    'version': '10.0.0.2.0',
+    'sequence': 14,
+    'category': 'Account',
+    'author': 'Consultoría Informática Studio 73 S.L.',
+    'summary': '',
+    'website': 'http://www.studio73.es',
+    'description': """
+Mostrar el botón 'Enviar por correo electrónico' de las facturas en los estados 'draft', 'open' y 'paid'
+""",
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/account_invoice_view.xml',
+    ],
+    'installable': True,
+}
