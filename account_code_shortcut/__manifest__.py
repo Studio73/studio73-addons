220c70e86dd840351ad16e61fe5b53bcb4a44073
Abraham Anes <abraham@studio73.es>
2018-01-18 10:58:39 +0100

    [MIG] account_code_shortcut: Migrado a v10.0.

--- account_code_shortcut/__manifest__.py
+++ account_code_shortcut/__manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# (c) 2017 Studio73 - Pablo Fuentes <pablo@studio73>
+# (c) 2017 Consultoría Informática Studio 73 S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Buscar cuentas con atajos',
-    'version': '1.0',
+    'version': '10.0.1.0.0',
     'category': 'Account',
     'sequence': 14,
     'description': """
@@ -13,6 +13,5 @@
     'author': 'Consultoría Informática Studio 73 S.L.',
     'website': 'http://www.studio73.es',
     'depends': ['account'],
-    'installable': False
+    'installable': True,
 }
-
