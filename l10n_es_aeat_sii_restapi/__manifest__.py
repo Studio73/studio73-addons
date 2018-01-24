e673026e12d7fc3510112e3f14e6affa399cd8bf
Ioan Galan <ioan@studio73.es>
2018-01-24 13:19:21 +0100

    [ADD] l10n_es_aeat_sii_restapi: Migrated to V10

---
+++ l10n_es_aeat_sii_restapi/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# (c) 2017 Studio73 - Pablo Fuentes <pablo@studio73>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Suministro Inmediato de Información RESTAPI",
+    "version": "10.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://odoo-community.org/",
+    "author": "Consultoría Informática Studio 73 S.L., "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es_aeat_sii",
+    ],
+    "data": [
+        'views/account_invoice_import_view.xml',
+        'views/account_invoice_import_actions.xml',
+        'security/ir.model.access.csv',
+    ],
+    "application": False,
+    "installable": True,
+}
---
+++ l10n_es_aeat_sii_restapi/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# (c) 2017 Consultoría Informática Studio 73 S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import account_invoice_import
+from . import aeat_sii_mapping_registration
