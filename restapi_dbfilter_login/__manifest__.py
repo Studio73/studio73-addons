48f35fe90854fa1813aceaef111085aa8925188e
Ioan Galan <ioan@studio73.es>
2018-01-25 17:24:36 +0100

    [WIP][ADD] restapi_dbfilter_login: Poder hacer login en Odoo sin tener que indicar la base de datos

---
+++ restapi_dbfilter_login/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# (c) 2017 Studio73 - Pablo Fuentes <pablo@studio73>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'REST API dbfilter Login',
+    'version': '8.0.1.0.0',
+    'description': """
+    Poder hacer login en Odoo sin tener que indicar la base de datos, útil para REST APIS
+    """,
+    'author': 'Consultoría Informática Studio 73 S.L.',
+    'website': 'http://www.studio73.es',
+    'depends': ['web', 'jsonrequest_exception_disable_traceback'],
+    'data': [],
+    'installable': True
+}
+
---
+++ restapi_dbfilter_login/controllers/main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# (c) 2017 Studio73 - Pablo Fuentes <pablo@studio73>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import http, tools
+from odoo.http import request
+from odoo.addons.web.controllers.main import Session
+
+
+class SessionCtrl(Session):
+
+    @http.route('/api/authenticate', type='json', auth="none")
+    def api_authenticate(self, login, password, db=False):
+        if not db:
+            db = request.httprequest.environ.get('HTTP_HOST', '').split(':')[0].replace('.', '-')
+        if not db:
+            return ''
+        request.session.authenticate(db, login, password)
+        return request.session_id
+
+    @http.route('/api/logout', type='json', auth="user")
+    def api_logout(self):
+        request.session.logout()
