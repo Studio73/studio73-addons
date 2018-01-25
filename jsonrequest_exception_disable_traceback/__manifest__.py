cfd31d5bb44f6542112b51b3e682d5e892234257
Ioan Galan <ioan@studio73.es>
2018-01-25 15:55:37 +0100

    [ADD] jsonrequest_exception_disable_traceback: En caso de error en una petición de tipo JSONRPC no devolver al usuario el traceback del error.

---
+++ jsonrequest_exception_disable_traceback/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# (c) 2017 Studio73 - Pablo Fuentes <pablo@studio73>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'jsonrequest exception disable traceback',
+    'version': '8.0.1.0.0',
+    'description': """
+    En caso de error en una petición de tipo JSONRPC no devolver al usuario el traceback del error. 
+    """,
+    'author': 'Consultoría Informática Studio 73 S.L.',
+    'website': 'http://www.studio73.es',
+    'depends': ['web'],
+    'data': [],
+    'installable': True
+}
+
