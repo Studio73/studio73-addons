25352e66dc55f53d4621ee339207b1cbe4c0268b
Ioan Galan <ioan@studio73.es>
2018-01-29 08:50:14 +0100

    [WIP][ADD] purchase_export_to_csv: Exportar pedidos de compra a CSV

---
+++ purchase_export_to_csv/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# (c) 2017 Consultoría Informática Studio73 SL (contacto@studio73.es)
+#          Ioan Galan <ioan@studio73.es>
+#
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
---
+++ purchase_export_to_csv/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase order to CSV',
+    'version': '10.0.0.1.0',
+    'sequence': 145,
+    'category': 'Purchase',
+    'author': 'Consultoría Informática Studio 73 S.L.',
+    'summary': 'Export purchase order to csv',
+    'website': 'http://www.studio73.es',
+    'description': """""",
+    'depends': [
+        'purchase',
+    ],
+    'data': [
+
+    ],
+    'installable': True,
+}
