b7d7e53c1afb89ca6493b95e95a059b272affefe
Guille <guillermo@studio73.es>
2019-07-11 15:47:07 +0200

    [MIG] account_code_shortcut: v12.0

--- account_code_shortcut/__manifest__.py
+++ account_code_shortcut/__manifest__.py
@@ -1,17 +1,16 @@
-# -*- coding: utf-8 -*-
-# (c) 2017 Consultoría Informática Studio 73 S.L.
+# Copyright 2017 Consultoría Informática Studio 73 S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Buscar cuentas con atajos',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Account',
     'sequence': 14,
-    'description': """
+    'summary': """
     Permite buscar cuentas contables utilizando atajos ej. 43.89 = 43000089 o 4300000089
     """,
     'author': 'Consultoría Informática Studio 73 S.L.',
-    'website': 'http://www.studio73.es',
+    'website': 'Studio73',
     'depends': ['account'],
     'installable': True,
 }
